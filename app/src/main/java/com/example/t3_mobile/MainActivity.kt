de07fd09af99223f4aff9aaade105514a5dc9345
stdfaza <myemail>
2026-04-20 14:48:08 +0800

    Nambah

--- app/src/main/java/com/example/t3_mobile/MainActivity.kt
+++ app/src/main/java/com/example/t3_mobile/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1. Inisialisasi View
         val tilNama = findViewById<TextInputLayout>(R.id.tilNama)
         val etNama = findViewById<TextInputEditText>(R.id.etNama)
         val rgKelamin = findViewById<RadioGroup>(R.id.rgKelamin)
@@ -22,32 +21,24 @@ class MainActivity : AppCompatActivity() {
         val btnTampilkan = findViewById<Button>(R.id.btnTampilkan)
         val tvHasil = findViewById<TextView>(R.id.tvHasil)
 
-        // 2. Aksi tombol diklik
         btnTampilkan.setOnClickListener {
 
-            // Reset pesan error setiap kali tombol diklik
             tilNama.error = null
 
             val nama = etNama.text.toString().trim()
             val selectedKelaminId = rgKelamin.checkedRadioButtonId
 
-            // --- PROSES VALIDASI ---
-
-            // Validasi 1: Cek apakah nama kosong
             if (nama.isEmpty()) {
-                tilNama.error = "Nama tidak boleh kosong" // Mengubah kotak jadi merah & memunculkan teks error
+                tilNama.error = "Nama tidak boleh kosong"
                 etNama.requestFocus()
                 Toast.makeText(this, "Nama tidak boleh kosong!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            // Validasi 2: Cek apakah jenis kelamin dipilih
             if (selectedKelaminId == -1) {
                 Toast.makeText(this, "Jenis kelamin harus dipilih!", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-
-            // --- PENGUMPULAN DATA JIKA VALIDASI LULUS ---
 
             val rbTerpilih = findViewById<RadioButton>(selectedKelaminId)
             val kelamin = rbTerpilih.text.toString()
@@ -59,14 +50,12 @@ class MainActivity : AppCompatActivity() {
 
             val hobi = if (daftarHobi.isNotEmpty()) daftarHobi.joinToString(", ") else "-"
 
-            // Format string output
             val hasilOutput = """
                 Nama    : $nama
                 Kelamin : $kelamin
                 Hobi    : $hobi
             """.trimIndent()
 
-            // Tampilkan ke TextView
             tvHasil.text = hasilOutput
             tvHasil.setTextColor(android.graphics.Color.WHITE)
         }
